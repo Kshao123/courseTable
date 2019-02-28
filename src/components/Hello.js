8af1feb310609ac128cc12cca734a1365c4a5359
jyk <1684502482@qq.com>
2019-02-28 15:50:58 +0800

    初始化

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render() {
+        return(
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+
+module.exports =  Hello;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Hello from './components/Hello.js';
+
+export default Hello
