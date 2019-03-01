dc465a70598cecc6f772f109851056ac83debe21
jyk <1684502482@qq.com>
2019-03-01 17:05:56 +0800

    初始化2

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface TableProps {
+    timetables?: any[];
+}
+
+export default class CourseTable extends React.PureComponent<TableProps, any> {}
---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import CourseTable from './CourseTable';
+//
+// export default CourseTable
+
+import React, { PureComponent } from 'react';
+// import DatePicker from 'antd/lib/date-picker'
+// import 'antd/lib/date-picker/style/index.css'
+// import 'antd/dist/antd.css'
+// import { DatePicker } from 'antd'
+
+class CourseTable extends PureComponent {
+    componentDidMount() {
+        console.log(123)
+    }
+
+    render() {
+        return(
+            <div>
+                new TimeTables
+                {/*<DatePicker />*/}
+            </div>
+        )
+    }
+}
+export default CourseTable;
